b0a4efb10b77fb0dc92743009db695549f43be0d
ilic93 <markoilic.va93@gmail.com>
2020-03-31 18:23:55 +0200

    header component added

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,39 @@
 <template>
-  <div class="calculator">
-    <div class="result">{{ this.result || 0 }}</div>
-    <div class="btn" @click="clear">AC</div>
-    <div class="btn" @click="prefix">+/-</div>
-    <div class="btn" @click="percent">%</div>
-    <div class="btn operator" @click="divide">/</div>
-    <div class="btn" @click="addNumber(7)">7</div>
-    <div class="btn" @click="addNumber(8)">8</div>
-    <div class="btn" @click="addNumber(9)">9</div>
-    <div class="btn operator" @click="multiply">*</div>
-    <div class="btn" @click="addNumber(4)">4</div>
-    <div class="btn" @click="addNumber(5)">5</div>
-    <div class="btn" @click="addNumber(6)">6</div>
-    <div class="btn operator" @click="substract">-</div>
-    <div class="btn" @click="addNumber(1)">1</div>
-    <div class="btn" @click="addNumber(2)">2</div>
-    <div class="btn" @click="addNumber(3)">3</div>
-    <div class="btn operator" @click="add">+</div>
-    <div class="btn zero" @click="addNumber(0)">0</div>
-    <div class="btn" @click="dot">.</div>
-    <div class="btn operator" @click="equal">=</div>
+  <div>
+    <Header :last="previous" :result="result"/>
+    <div class="calculator">
+      <div class="result">{{ result || 0 }}</div>
+      <div class="btn" @click="clear">AC</div>
+      <div class="btn" @click="prefix">+/-</div>
+      <div class="btn" @click="percent">%</div>
+      <div class="btn operator" @click="divide">/</div>
+      <div class="btn" @click="addNumber(7)">7</div>
+      <div class="btn" @click="addNumber(8)">8</div>
+      <div class="btn" @click="addNumber(9)">9</div>
+      <div class="btn operator" @click="multiply">*</div>
+      <div class="btn" @click="addNumber(4)">4</div>
+      <div class="btn" @click="addNumber(5)">5</div>
+      <div class="btn" @click="addNumber(6)">6</div>
+      <div class="btn operator" @click="substract">-</div>
+      <div class="btn" @click="addNumber(1)">1</div>
+      <div class="btn" @click="addNumber(2)">2</div>
+      <div class="btn" @click="addNumber(3)">3</div>
+      <div class="btn operator" @click="add">+</div>
+      <div class="btn zero" @click="addNumber(0)">0</div>
+      <div class="btn" @click="dot">.</div>
+      <div class="btn operator" @click="equal">=</div>
+    </div>
   </div>
 </template>
 
 <script>
+import Header from './components/Header'
 
 export default {
   name: 'App',
+  components: {
+    Header
+  },
   data() {
     return {
       result: '',
@@ -60,21 +67,25 @@ export default {
     },
     add() {
       if(this.previous && this.operator) {this.equal()}
+
       this.setData()
       this.operator = (a, b) => a + b
     },
     substract() {
       if(this.previous && this.operator) {this.equal()}
+
       this.setData()
       this.operator = (a, b) => a - b
     },
     multiply() { 
       if(this.previous && this.operator) {this.equal()}
+
       this.setData()
       this.operator = (a, b) => a * b
     },
     divide() {
       if(this.previous && this.operator) {this.equal()}
+      
       this.setData()
       this.operator = (a, b) => a / b
     },
@@ -101,7 +112,7 @@ body {
 .calculator {
   width: 500px;
   border: 2px solid black;
-  margin: 0 auto;
+  margin: 20px auto;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-auto-rows: 50px;
